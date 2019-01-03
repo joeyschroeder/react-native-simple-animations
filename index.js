bb5b0796fb3eeb2d57fdc98905e7c0891d4094f7
Joey Schroeder <schrjos@gmail.com>
2019-01-03 12:23:43 -0600

    Update deployment scripts

--- index.js
+++
@@ -1,7 +0,0 @@
-import { AIMS } from './src/constants/aims';
-import { DIRECTIONS } from './src/constants/directions';
-import { MOVEMENT_TYPES } from './src/constants/movement-types';
-import { STATIC_TYPES } from './src/constants/static-types';
-import { SimpleAnimation } from './src/simple-animation';
-
-export { AIMS, DIRECTIONS, MOVEMENT_TYPES, STATIC_TYPES, SimpleAnimation };
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { AIMS } from './constants/aims';
+import { DIRECTIONS } from './constants/directions';
+import { MOVEMENT_TYPES } from './constants/movement-types';
+import { STATIC_TYPES } from './constants/static-types';
+import { SimpleAnimation } from './simple-animation';
+
+export { AIMS, DIRECTIONS, MOVEMENT_TYPES, STATIC_TYPES, SimpleAnimation };
